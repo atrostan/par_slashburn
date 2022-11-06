3b6b3b3aa2425708642b46a0fb6213387882d9ba
atrostan <atrostan@cs.ubc.ca>
2022-11-05 18:16:45 -0700

    downgrade cmake version; assert all vertices have been assigned a slashburn index; convert map of vertex degree decrements to pvector in parallel; don't sort by original vertex id within a spoke -   as described in original slashburn pub; if directed, read graph as directed + don't symmetrize; revert to original sample size for afforest;

--- main.cpp
+++ main.cpp
@@ -18,18 +18,46 @@ bool degree_compare(const std::pair<uint64_t, uint64_t> &a, const std::pair<uint
 	return a.second > b.second;
 }
 
+bool valid_perm(pvector<uint64_t> &perm) {
+	pvector<uint64_t> copy(perm.begin(), perm.end());
+	ips4o::parallel::sort(
+		copy.begin(),
+		copy.end()
+	);
+	std::vector<bool> all_valid(omp_get_max_threads(), true);
+
+#pragma omp parallel for schedule(static)
+	for (uint64_t i = 0; i < copy.size(); i++)
+		if (copy[i] != i) {
+			fmt::print("Thread {} found unassigned vertex: {}: {}\n", omp_get_thread_num(), i);
+			all_valid[omp_get_thread_num()] = false;
+		}
+
+	return std::all_of(all_valid.begin(), all_valid.end(), [](bool v) { return v; });
+}
+
 int main(int argc, char *argv[]) {
 	CLApp cli(argc, argv, "connected-components-afforest");
 	if (!cli.ParseArgs())
 		return -1;
 	Builder b(cli);
 	fmt::print("cli.out_filename(): {}\n", cli.out_filename());
+	fmt::print("cli.symmetrize(): {}\n", cli.symmetrize());
 	Graph g = b.MakeGraph();
+	fmt::print("g.num_nodes(): {}\n", g.num_nodes());
+	fmt::print("g.num_edges(): {}\n", g.num_edges());
+	fmt::print("g.num_edges_directed(): {}\n", g.num_edges_directed());
+	fmt::print("g.directed: {}\n", g.directed());
 	int n_neighbour_rounds = 2;
 	float percent = cli.percent();
-  // TODO add k as input arg
+	// TODO add k as input arg
 	Bitmap bmap(g.num_nodes());
 	SlashBurn sb = SlashBurn(g, n_neighbour_rounds, percent, bmap);
+
+	// check validity by sorting the permutation array and checking that all [0, n) vertices
+	// have been assigned a slashburn index
+	assert(valid_perm(sb.perm));
+
 	sb.write_permutation(cli.out_filename());
 
 	boost::filesystem::path p(cli.filename());
